dbf0f900a2a425f89fc83e755f4b2c08a9b88900
m0rgause <antoresf1@gmail.com>
2025-07-17 12:34:16 +0700

    fix: update import path for axios service in mangas controller

--- src/controller/mangas.ts
+++ src/controller/mangas.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from "express";
-import { get } from "@/services/axios";
+import { get } from "../services/axios";
 import { ApiResponse } from "../middleware";
 const cheerio = require("cheerio");
 
